a00ed9deb0ed695904f1a89ae8b51a721860fac0
dzzirima@gmail.com <dzzirima@gmail.com>
2021-06-27 15:10:25 +0200

    basic functionality in use now

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+// this file will be uses for the configuration of  of routing processs
+import express from 'express'
+const router = express.Router()
+
+import {register, forgotpassword, resetpassword, login} from '../controllers/auth.js'
+
+router.route("/register").post(register) 
+// this is the same  as the following router.post("/register",register)\
+router.route("/login").post(login)
+
+router.route("/forgotpassword").post(forgotpassword)
+router.route("/resetpassword/:resetToken").post(resetpassword)
+
+
+export default router
